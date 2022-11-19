6abac9b6679dba870efdf2d981378dad6b7ea85c
harc06 <harc6@hotmail.com>
2022-11-19 10:13:09 -0600

    [REF]Refactor for the getGifs function

--- MidudevReact/giffy/src/services/getGifs.js
+++ MidudevReact/giffy/src/services/getGifs.js
@@ -1,18 +1,14 @@
 const apiKey = '6AA66W26AqqnHFFLg8SqjGwX95Ti8IG7'
 
-export default function gefGifs(keyword = 'dog') {
+export default async function getGifs2(keyword = '404') {
     const url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${keyword}&limit=12&offset=0&rating=r&lang=en`
-    return fetch(url)
-            .then(res => res.json())
-            .then(response => {
-                const { data } = response
-                if(Array.isArray(data)) {
-                    const gifs = data.map(image => {
-                        const { title, id, images } = image
-                        const { url } = images.fixed_width
-                        return {title, id , url}
-                    })
-                    return gifs
-                }
-            })
+    const fetching = await fetch (url)
+    const response = await fetching.json()
+    const { data } = response 
+    const gifs = data.map(image => {
+      const { title, id, images } = image
+      const { url } = images.fixed_width
+      return {title, id , url}
+    })
+    return gifs
 }
